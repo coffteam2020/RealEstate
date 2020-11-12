1066a8b3b9557b368822c2d840d23a7895288d77
Nguyen Dang Ha <dha.k9tt@gmail.com>
2020-11-12 11:28:32 +0700

    upload

--- Mobile/js/containers/RestaurantScreen/index.js
+++ Mobile/js/containers/RestaurantScreen/index.js
@@ -63,6 +63,7 @@ export default function RestaurantScreen(props) {
                     phone: phone,
                     owner: owner,
                     avatar: urlState,
+                    title: "",
                     menu: [
                         {
                             id: 1,
